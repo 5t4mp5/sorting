b9fc65a3a419877b38dfc4ec0b088f14142983dc
Admin <admin@Admins-MacBook-Pro.local>
2019-01-15 20:59:10 -0500

    with custom func

--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -12,3 +12,16 @@ describe('Bubble Sort', function(){
     expect(bubbleSort([1,3,4,6,5,2, 76, 22, 34, 11, 109] )).toEqual([1,2,3,4,5,6,11,22,34,76,109]);
   });
 });
+
+describe('customBubble', () => {
+  it('sorts with a custom func', () =>{
+    var arrToSort = [{ age: 4 }, { age: 8 }, { age: 2 }, { age: 9 }];
+    var sorted = arrToSort.sort(function comparator (a, b) {
+      if (a.age < b.age) return -1; // returning `-1` means "a goes before b"
+      if (a.age > b.age) return 1;  // returning  `1` means "b goes before a"
+      return 0; // returning 0 means "a and b are equivalent"
+    });
+
+    expect(sorted).toEqual([{age: 2}, {age: 4}, {age: 8}, {age: 9}]);
+  });
+});
